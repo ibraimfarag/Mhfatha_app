d41672a08a0d2fe3649823b89e359082ab2ebf03
ibrahim ahmed <ib.farag@gmail.com>
2024-02-19 22:25:40 +0200

    updates

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by ibrahim farag ali ahmed on 19/02/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
